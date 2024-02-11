9f368b51be7df52614c3ef8d86cb902047ff46e0
Vijay Krishna K <112117049+vijaykrishnak04@users.noreply.github.com>
2024-02-11 23:41:49 +0530

    fixed login issue

--- src/use_cases/user/Login.ts
+++ src/use_cases/user/Login.ts
@@ -2,17 +2,27 @@
 
 import { generateToken } from "../../utils/tokenUtils";
 import { IUserRepository } from "../../repositories/UserRepository";
-import { IUser } from "src/entities/User";
 
 export class LoginUseCase {
   constructor(private userRepository: IUserRepository) {}
 
-  async execute(email: string, password: string): Promise<{ username: string; email: string; userId: string; token: string; }> {
+  async execute(
+    email: string,
+    password: string
+  ): Promise<{
+    username: string;
+    email: string;
+    userId: string;
+    token: string;
+  }> {
     const user = await this.userRepository.findByEmail(email);
-    if (
-      !user ||
-      !this.userRepository.isValidPassword(password, user.password)
-    ) {
+
+    const passwordVerify: boolean = await this.userRepository.isValidPassword(
+      password,
+      user.password
+    );
+
+    if (passwordVerify === false) {
       throw new Error("Invalid username or password.");
     }
 
